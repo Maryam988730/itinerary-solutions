256d24030a8068f1b1bc3fe185c83de92b80c85c
Maryam Ahmed <maryam.ahmed-7@student.manchester.ac.uk>
2025-11-08 16:39:21 +0000

    fixed database

--- login-register/app/models.py
+++ login-register/app/models.py
@@ -6,7 +6,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
